4c9dfbaf0b80ff852e75726dc5ea63ad069708ad
Ivan Vahanov <ivan.seeker6@gmail.com>
2024-08-16 17:10:58 +0300

    sol1

--- taxi/tests.py
+++ taxi/tests.py
@@ -22,6 +22,8 @@ class SearchFeatureTests(TestCase):
             license_number="XYZ67890"
         )
 
+        self.client.login(username='john_doe', password='password123')
+
     def test_search_manufacturer(self):
         response = self.client.get(
             reverse("taxi:manufacturer-list"), {"q": "Toyota"})
